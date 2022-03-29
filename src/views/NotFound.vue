c82385cc8951ce9d36e62dbb8d3791e42f8e10f6
haust0301 <phanthanhhaust@gmail.com>
2022-03-29 22:46:19 +0700

    Cai dat trang loi 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="page">
         <p>
-            Oops, không thể tìm thấy trang. Trở về
+            Oopss, không thể tìm thấy trang. Trở về
             <router-link to="/">trang chủ.</router-link>
         </p>
     </div>
